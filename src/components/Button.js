3ce14fd6818aa733c2b32b830057d573756496d4
dganai <dinaganai@gmail.com>
2022-02-24 15:40:11 -0500

    setup of DayListItem component

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,11 +5,15 @@ import 'components/Button.scss';
 export default function Button(props) {
   const buttonClass = classNames('button', {
     'button--confirm': props.confirm,
-    'button-danger': props.danger,
+    'button--danger': props.danger,
   });
 
   return (
-    <button onClick={props.onClick} className={buttonClass} disabled={disabled}>
+    <button
+      onClick={props.onClick}
+      className={buttonClass}
+      disabled={props.disabled}
+    >
       {props.children}
     </button>
   );
