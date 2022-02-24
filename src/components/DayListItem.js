9b95b5e92af9e5bb37fc7b1442c8a864b7164c77
dganai <dinaganai@gmail.com>
2022-02-24 15:50:12 -0500

    implementing props in DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => setDay(name)} selected={selected}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{spots} spots remaining</h3>
     </li>
   );
 }
