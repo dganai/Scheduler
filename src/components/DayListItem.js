490d654c9f3f0549d892361c3f846976fa664994
dganai <dinaganai@gmail.com>
2022-02-24 16:00:47 -0500

    updating components to apply scss styling from DayListItem.scss

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import classNames from 'classnames';
-import DayListItem from 'components/DayListItem.scss';
+import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
   const { name, spots, selected, setDay } = props;
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': props.spots === 0,
+  });
 
   return (
-    <li onClick={() => setDay(name)} selected={selected}>
+    <li className={dayClass} onClick={() => setDay(name)} selected={selected}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>
