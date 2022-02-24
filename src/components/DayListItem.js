65c0184c677b2f7a3c0211b225ce6815a66b0637
dganai <dinaganai@gmail.com>
2022-02-24 16:01:52 -0500

    refactor

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,7 +10,7 @@ export default function DayListItem(props) {
   });
 
   return (
-    <li className={dayClass} onClick={() => setDay(name)} selected={selected}>
+    <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>
