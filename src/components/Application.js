4eee63d4927b081e93a652ed73b686b26c1aec0e
dganai <dinaganai@gmail.com>
2022-02-25 11:42:30 -0500

    adding state to application component and passing to DayList component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import 'components/Application.scss';
 import DayList from './DayList';
@@ -22,6 +22,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState('Monday');
   return (
     <main className="layout">
       <section className="sidebar">
@@ -32,11 +33,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            day={'Monday'}
-            setDay={(day) => console.log(day)}
-          />
+          <DayList days={days} day={DayList} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
