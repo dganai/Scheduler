00adc4ab27677c2da1fde338c8a8c9da3426caba
dganai <dinaganai@gmail.com>
2022-02-25 10:54:34 -0500

    new component DayList to hold all DayListItem components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,2 +1,17 @@
-import React from "react";
-import DayListItem from "./DayListItem";
+import React from 'react';
+import DayListItem from './DayListItem';
+
+export default function DayList(props) {
+  const { days, day, setDay } = props;
+
+  const newDaysArr = days.map((elems) => (
+    <DayListItem
+      key={elems.id}
+      name={elems.name}
+      spots={elems.spots}
+      selected={elems.day}
+      setDay={elems.setDay}
+    ></DayListItem>
+  ));
+  return <ul>{newDaysArr}</ul>;
+}
