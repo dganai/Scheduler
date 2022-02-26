6a128cdb6f06bd0c86839ac417a40d3f180e03dc
dganai <dinaganai@gmail.com>
2022-02-26 17:11:21 -0500

    conditionally add classes

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,8 +5,12 @@ import classNames from 'classnames';
 export default function setInterviewer(props) {
   const { id, name, avatar, selected, setInterviewer } = props;
 
+  const interviewersClass = classNames('interviewers__item', {
+    'interviewers__item--selected': selected,
+  });
+
   return (
-    <li onClick={setInterviewer} className="interviewers__item">
+    <li onClick={setInterviewer} className={interviewersClass}>
       <img className={`interviewers__item-image`} src={avatar} alt={name} />
       {selected && name}
     </li>
