d5ccbc320dd00b619e81c5cf3f349f0e6d3d1429
dganai <dinaganai@gmail.com>
2022-02-26 17:20:46 -0500

    setup for InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function setInterviewer(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}
