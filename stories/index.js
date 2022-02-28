081984bc04966f05619e5535cecbb44884325edb
dganai <dinaganai@gmail.com>
2022-02-28 11:26:05 -0500

    add story for error component

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 storiesOf('Button', module)
   .addParameters({
@@ -155,4 +156,7 @@ storiesOf('Appointment', module)
       onConfirm={action('onConfirm}')}
     />
   ))
-  .add('Status', () => <Status message="Deleting" />);
+  .add('Status', () => <Status message="Deleting" />)
+  .add('Error', () => (
+    <Error message="Could not delete appointment" onClose={action('onClose')} />
+  ));
