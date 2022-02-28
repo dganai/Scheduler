2227ea10157958f1f64b0893c1c18e2b4202d9e1
dganai <dinaganai@gmail.com>
2022-02-28 10:42:14 -0500

    import Appointment component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
+import Appointment from 'components/Appointment/index.js';
 
 storiesOf('Button', module)
   .addParameters({
