a3545072a75bd0c6b45e3060f668e7e1d33bc451
dganai <dinaganai@gmail.com>
2022-02-28 12:17:03 -0500

    add story for form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,44 +1,48 @@
-import React, { useState } from "react";
-import InterviewerList from "components/InterviewerList";
-import Button from "components/Button";
+import React, { useState } from 'react';
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
 
 export default function Form(props) {
-  const {interviewers, onSave, onCancel} = props
-  const [student, setStudent] = useState(props.studnet || "")
-  const [interviewer, setInterviewer] = useState(props.interviewer || null)
+  const { interviewers, onSave, onCancel } = props;
+  const [student, setStudent] = useState(props.studnet || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const reset = () => {
-    setStudent("")
-    setInterviewer(null)
-  }
+    setStudent('');
+    setInterviewer(null);
+  };
   const cancel = () => {
-    reset()
-    onCancel()
-  }
+    reset();
+    onCancel();
+  };
   return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form onSubmit={(event) => event.preventDefault()} autoComplete="off">
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        value={student}
-        onChange={(event) => setStudent(event.target.value)}
-      />
-    </form>
-    <InterviewerList 
-      value={interviewer}
-      interviewers={interviewers}
-      onChange={setInterviewer}
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={onSave}>Save</Button>
-    </section>
-  </section>
-</main>
-  )
+      <section className="appointment__card-left">
+        <form onSubmit={(event) => event.preventDefault()} autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          value={interviewer}
+          interviewers={interviewers}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={onSave}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
 }
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
 
 storiesOf('Button', module)
   .addParameters({
@@ -159,4 +160,11 @@ storiesOf('Appointment', module)
   .add('Status', () => <Status message="Deleting" />)
   .add('Error', () => (
     <Error message="Could not delete appointment" onClose={action('onClose')} />
+  ))
+  .add('Create', () => (
+    <Form
+      interviewers={interviewers}
+      onCancel={action('onCancel')}
+      onSave={action('onSave')}
+    />
   ));
