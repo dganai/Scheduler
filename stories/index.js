71836b01a6ab6f47f259d5adcc166cf6a913124f
dganai <dinaganai@gmail.com>
2022-02-28 12:52:46 -0500

    story for appointments booked

--- stories/index.js
+++ stories/index.js
@@ -182,4 +182,14 @@ storiesOf('Appointment', module)
       <Appointment id={1} time="4pm" />
       <Appointment time="5pm" />
     </Fragment>
+  ))
+  .add('Appointment Booked', () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: 'Lydia Miller-Jones', interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
