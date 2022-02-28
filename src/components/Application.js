264adaa27499c90f9db77808448b7b522c4d5bbe
dganai <dinaganai@gmail.com>
2022-02-28 13:00:04 -0500

    adding mock data for appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,46 @@ import React, { useState } from 'react';
 
 import 'components/Application.scss';
 import DayList from './DayList';
+import Appointment from './Appointment';
+
+const appointments = [
+  {
+    id: 1,
+    time: '12pm',
+  },
+  {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 3,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 3,
+    time: '2pm',
+  },
+  {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: 'Archie Andrews',
+      interviewer: {
+        id: 4,
+        name: 'Cohana Roy',
+        avatar: 'https://i.imgur.com/FK8V841.jpg',
+      },
+    },
+  },
+  {
+    id: 5,
+    time: '4pm',
+  },
+];
 
 const days = [
   {
@@ -42,7 +82,10 @@ export default function Application(props) {
         />{' '}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => {
+          return <Appointment key={appointment.id} {...appointment} />;
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
