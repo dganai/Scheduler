55cd773ee781ba6f646ef4f8348b3d201fe77e72
dganai <dinaganai@gmail.com>
2022-03-01 16:03:37 -0500

    add useVisualMode logic to component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,25 @@ import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import { useVisualMode } from 'components/hooks/useVisualMode';
+
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
 
 export default function Appointment(props) {
   const { id, interview, time } = props;
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? (
-        <Show student={interview.student} interviewer={interview.interviewer} />
-      ) : (
-        <Empty />
+      {mode === EMPTY && <Empty onAdd={() => console.log('Clicked onAdd')} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
       )}
     </article>
   );
