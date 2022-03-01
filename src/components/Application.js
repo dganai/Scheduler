4515e5e1a46ce88886b312fb7f1531f9ee1976c5
dganai <dinaganai@gmail.com>
2022-03-01 15:28:16 -0500

    implement getInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from './Appointment';
-import { getAppointmentsForDay } from 'helpers/selectors';
+import { getAppointmentsForDay, getInterview } from 'helpers/selectors';
 
 export default function Application() {
   const [state, setState] = useState({
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -5,3 +5,12 @@ export function getAppointmentsForDay(state, day) {
     ? findDay.appointments.map((id) => state.appointments[id])
     : [];
 }
+
+export function getInterview(state, interview) {
+  return (
+    interview && {
+      studnet: interview.student,
+      interviewer: { ...state.interviewers[interview.interviewer] },
+    }
+  );
+}
