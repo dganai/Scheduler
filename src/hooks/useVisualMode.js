5afca7277ebefb93a022f78fde05ac5ec57ad6da
dganai <dinaganai@gmail.com>
2022-03-01 15:54:40 -0500

    transition to another mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,15 @@ import { useState } from 'react';
 
 export default function useVisualMode(inital) {
   const [mode, setMode] = useState(inital);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  const transition = (mode, replace = false) => {
+    if (replace) {
+      history.pop();
+    }
+    history.push(mode);
+    setMode(mode);
+  };
+
+  return { mode, transition };
 }
