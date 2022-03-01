e87501ebdf3036c282d4ffba2d1a84164061b3e1
dganai <dinaganai@gmail.com>
2022-03-01 17:42:30 -0500

    cancel/delete appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,14 +5,18 @@ import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
+import Confirm from './Confirm';
 import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
-  const { id, time, interview, interviewers, bookInterview } = props;
+  const { id, time, interview, interviewers, bookInterview, cancelInterview } =
+    props;
   const EMPTY = 'EMPTY';
   const SHOW = 'SHOW';
   const CREATE = 'CREATE';
   const SAVING = 'SAVING';
+  const DELETE = 'DELETE';
+  const CONFIRM = 'CONFIRM';
 
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
@@ -24,12 +28,22 @@ export default function Appointment(props) {
     transition(SAVING);
     bookInterview(id, interview).then(() => transition(SHOW));
   }
+
+  function cancel() {
+    transition(DELETE);
+    cancelInterview(id).then(() => transition(EMPTY));
+  }
+
   return (
     <article className="appointment">
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
-        <Show student={interview.student} interviewer={interview.interviewer} />
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
+        />
       )}
       {mode === CREATE && (
         <Form
@@ -38,7 +52,14 @@ export default function Appointment(props) {
           onSave={save}
         />
       )}
-      {mode === SAVING && <Status message={SAVING} />}
+      {mode === SAVING && <Status message="Saving" />}
+      {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you would like to delete?"
+          onConfirm={() => cancel()}
+        />
+      )}
     </article>
   );
 }
