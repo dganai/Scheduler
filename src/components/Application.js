d71f588e9d382dc69a52a34e2141844946b7fc89
dganai <dinaganai@gmail.com>
2022-03-02 12:36:37 -0500

    moving logic from Application.js into new hook file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from './Appointment';
@@ -8,49 +7,11 @@ import {
   getInterview,
   getInterviewersForDay,
 } from 'helpers/selectors';
+import useApplicationData from './hooks/useApplicationData';
 
-export default function Application() {
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {},
-  });
-
-  const setDay = (day) => setState({ ...state, day });
-
-  const appointments = getAppointmentsForDay(state, state.day);
-  const interviewers = getInterviewersForDay(state, state.day);
-  useEffect(() => {
-    Promise.all([axios.get('/api/days'), axios.get('/api/appointments')]),
-      axios.get('/api/interviewers').then((all) => {
-        setState((prev) => ({
-          ...prev,
-          days: all[0].data,
-          appointments: all[1].data,
-          interviewers: all[2].data,
-        }));
-      });
-  }, []);
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    return axios.put(`/api/appointments/${id}`, appointment).then((res) => {
-      setState({
-        ...state,
-        appointments,
-      });
-      console.log(res);
-    });
-  }
+export default function Application(props) {
+  const { state, setDay, bookInterview, cancelInterview } =
+    useApplicationData();
 
   return (
     <main className="layout">
@@ -72,7 +33,7 @@ export default function Application() {
       </section>
       <section className="schedule">
         {appointments.map((appointment) => {
-          const schedule = appointments.map((appointment) => {
+          const dayappointments = appointments.map((appointment) => {
             const interview = getInterview(state, appointment.interview);
 
             return (
@@ -83,10 +44,12 @@ export default function Application() {
                 interview={interview}
                 interviewers={interviewers}
                 bookInterview={bookInterview}
+                cancelInterview={cancelInterview}
               />
             );
           });
         })}
+        {dayappointments}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
